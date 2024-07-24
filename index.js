856311ec87fb8f10af1b8f2032d02635c50b3e10
Lewis Odero <lewisodero27@gmail.com>
2024-07-24 10:44:10 +0300

    test: pass tests for making of a function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+// Code your solution in this file
+
+const distanceFromHqInBlocks = (block) => {
+  return Math.abs(block - 42);
+}
+
+const distanceFromHqInFeet = (block) => {
+        return distanceFromHqInBlocks(block) * 264;
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+  return Math.abs(start - destination) * 264;
+}
+
+const calculatesFarePrice = (start, destination) => {
+        const distance = distanceTravelledInFeet(start, destination);
+        if (distance <= 400) {
+        return 0;
+        } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+        } else if (distance > 2000 && distance < 2500) {
+        return 25;
+        } else {
+        return 'cannot travel that far';
+        }
+}
+
+
+
+
+
+ 
+
